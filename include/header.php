0b6bcf50924c9967fc8884d2b94b7b2d3f8a1639
Quang Hà Nguyễn <nguyenquangha130901@gmail.com>
2023-11-03 11:32:01 +0700

    code giao dien NVTD

---
+++ include/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Trang Chủ</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./Font/fontawesome-free-6.4.2-web/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container-fluid bg-light hi">
+        <h6>Chào mừng bạn đến với bếp ăn tập đoàn!</h6>
+    </div>
+    <header>
+        <div class="container-fluid ">
+            <div class="row main-header">
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <a href="index.html"><img class="logo" src="./img/LOGO-01.png" alt="Logo" /></a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 time">
+                    <div class="clock">
+                        <i class="fa-regular fa-clock " style="color: #080808; font-size: 50px;"></i>
+                    </div>
+                    <div class="time-content">
+                        <span class="time-title">OPEN:</span><br>
+                        <span class="time-info">10AM - 17PM</span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 user">
+                    <i class="fa-solid fa-user fa-2xl"></i>
+                    <div class="title-user">
+                        <ul class="nav">
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-toggle" data-bs-toggle="dropdown" style="color: #080808; text-decoration: none;" href="#">User</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Xem Phiếu đặt món</a></li>
+                                    <li><a class="dropdown-item" href="#">Xem công nợ</a></li>
+                                    <li><a class="dropdown-item" href="#">Đề xuất món</a></li>
+                                    <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
